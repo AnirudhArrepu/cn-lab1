05d4640367f15ed8ad39987cec0d4aba9b4ed856
anirudharrepu <anirudharrepu@gmail.com>
2026-01-06 15:15:17 +0530

    [lab1] plotting signals

--- cs23b008_nrz_am.py
+++ cs23b008_nrz_am.py
@@ -11,16 +11,38 @@ msg = nrz(seq, 200)
 print(f"nrz encoding for seq {seq}: {msg}")
 print(len(msg))
 
-def sin_carrier_wave(fc: int, dur:int = 0.1, sam_rate: int =1000)->np.array:
+def sin_carrier_wave(fc: int, dur:int = 5, sam_rate: int =1000)->np.array:
     x = np.linspace(0, dur, int(sam_rate*dur))
     y = np.sin(2*np.pi*fc*x)
     return y
 
 sin_wave = sin_carrier_wave(20)
+print(f"sin carrier wave: {sin_wave}")
 
 def amp_mod(sin_wave: np.array, msg_nrz: np.array)->np.array:
     sin_wave = np.repeat(sin_wave, int(np.ceil(len(msg_nrz)/len(sin_wave))))[:len(msg_nrz)]
     return sin_wave*msg_nrz
 
 amp_wave = amp_mod(sin_wave, msg)
+print(f"am modulated signal: {amp_wave}")
 
+import matplotlib.pyplot as plt
+# plt.title("cs23b008")
+plt.figure(figsize=(10,10))
+
+plt.subplot(3,1,1)
+plt.title("msg signal")
+plt.grid(True)
+plt.plot(msg)
+
+plt.subplot(3,1,2)
+plt.title("carrier signal")
+plt.grid(True)
+plt.plot(sin_wave)
+
+plt.subplot(3,1,3)
+plt.title("am signal")
+plt.grid(True)
+plt.plot(amp_wave)
+
+plt.savefig("amplots.pdf")
